a28f1dd09f471984d923ca6f40c04cf5b2e8b0d9
Ilyusha <ilyailiilia@gmail.com>
2023-02-10 20:18:00 +0100

    Implement basic cutting interface

--- cutter/sample_cut_tool.py
+++ cutter/sample_cut_tool.py
@@ -22,26 +22,17 @@ class MP3SampleCutTool:
 
         # Create a loop to continuously check for user commands
         while picking:
+            # Make sure that the current position in not out of bounds
+            if current_position < 0:
+                current_position = 0
+            if current_position > len(self.audio):
+                current_position = len(self.audio) - length
+            print("Current position: " + str(current_position / 1000))
             # Get the user command
-            command = input("Commands: "
-                            "b (time_sec)\n"
-                            "p\n"
-                            "f\n" 
-                            "r\n"
-                            "l (time_sec)\n"
-                            "s (time_sec)\n"
-                            "plot \n"
-                            "cut \n"
-                            "q \n"
-                            "You command:>>>")
+            command = input(">>>")
 
             # Start the playback
             if command.startswith("p") and command != "plot":
-                # Check if play command has time argument
-                if len(command.split(" ")) > 1 and command.split(" ")[1].isdigit():
-                    print("Playing from " + command.split(" ")[1] + " seconds")
-                    current_position = int(command.split(" ")[1])
-
                 pydub.playback.play(self.audio[current_position:current_position + length])
 
             # Set the beginning of the playback
@@ -64,16 +55,39 @@ class MP3SampleCutTool:
                     # If the current position + length is longer than the audio, set the length to the end of the audio
                     if current_position + length > len(self.audio):
                         length = len(self.audio) - current_position
+                print("Length: " + str(length / 1000))
 
             # Set the step of the playback
             elif command.startswith("s"):
                 # Ensure that the length is a number
                 if len(command.split(" ")) > 1 and command.split(" ")[1].isdigit():
                     step = int(command.split(" ")[1]) * 1000
+                print("Step: " + str(step / 1000))
 
             # Forward the playback
             elif command.startswith("f"):
-                current_position += step
+                # If every letter in command is "f", count the number of "f"s
+                if len(command) == command.count("f"):
+                    current_position += step * len(command)
+
+            # Rewind the playback
+            elif command.startswith("r"):
+                # If every letter in command is "r", count the number of "r"s
+                if len(command) == command.count("r"):
+                    current_position -= step * len(command)
+
+            elif command.startswith("help"):
+                print("p - play selected to cut part of the track")
+                print("b (seconds) - set beginning of the sample")
+                print("l (seconds) - set length of the sample")
+                print("s (seconds) - set step for forward and rewind")
+                print("f - forward. You can use multiple f's to fast forward (e.g. fff - fast forward 3 times)")
+                print("r - rewind. You can use multiple r's to rewind (e.g. rrr - rewind 3 times)")
+                print("plot - plot amplitude of the selected part of the track")
+                print("info - print information about cutting the track")
+                print("cut - cut the track")
+                print("cut -a - cut the track and adjust the cut position")
+                print("q - quit")
 
             elif command.startswith("plot"):
                 # Extract the samples for the selected part of the track
@@ -86,9 +100,11 @@ class MP3SampleCutTool:
                 plt.ylabel("Amplitude")
                 plt.show()
 
-            # Rewind the playback
-            elif command.startswith("r"):
-                current_position -= step
+            # Print the information about the audio file
+            elif command.startswith("info"):
+                print("Current position: " + str(current_position / 1000))
+                print("Length: " + str(length))
+                print("Step: " + str(step))
 
             # Quit the cut tool
             elif command.startswith("q"):
@@ -96,13 +112,55 @@ class MP3SampleCutTool:
 
             # Cut the track
             elif command.startswith("cut"):
-                self.cut_track(current_position, current_position + length, length)
+                if " -a" in command:
+                    self._cut_track(current_position, length, adjust_cut_position=True)
+                else:
+                    self._cut_track(current_position, length, adjust_cut_position=False)
 
-    def cut_track(self, start_cut, end_cut, length):
+    def _cut_track(self, start_cut, length, adjust_cut_position=False):
         # Cut the track using the selected cut points
+        if adjust_cut_position:
+            start_cut = self._adjust_cut_position(start_cut, length)
+
+        end_cut = start_cut + length
         cut_audio = self.audio[start_cut:end_cut]
-        original_name = os.path.basename(self.audio_file_path)
+        original_name = os.path.basename(self.audio_file_path).split(".")[0]
         sample_file_name = original_name + "_" + str(start_cut) + "_" + str(length) + ".mp3"
         cut_audio.export(sample_file_name, format="mp3")
         print("Saved " + sample_file_name)
+
+    def _adjust_cut_position(self, current_position, length, threshold=0.05):
+        # Extract the samples for the selected part of the track
+        selected_samples = self.audio[current_position:current_position + length].get_array_of_samples()
+
+        # Calculate the volume levels for the extracted samples
+        volume_levels = [abs(sample) for sample in selected_samples]
+
+        # Normalize the volume levels to be in the range of 0-100
+        normalized_volume_levels = [level / (2 ** 15) * 100 for level in volume_levels]
+
+        # Find the local maxima of the volume levels
+        maxima = []
+        for i in range(1, len(normalized_volume_levels) - 1):
+            if normalized_volume_levels[i] > normalized_volume_levels[i - 1] and \
+                    normalized_volume_levels[i] > normalized_volume_levels[i + 1]:
+                maxima.append(i)
+
+        # Select the local maxima closest to the original cut position
+        closest_maxima = None
+        closest_distance = float("inf")
+        for max_index in maxima:
+            distance = abs(max_index - length // 2)
+            if distance < closest_distance:
+                closest_distance = distance
+                closest_maxima = max_index
+
+        # Adjust the cut position to be at the selected local maxima
+        if closest_maxima is not None and closest_distance / length > threshold:
+            adjusted_position = current_position + closest_maxima - length // 2
+            print("Adjusted cut position from " + str(current_position) + " to " + str(adjusted_position))
+            return adjusted_position
+        else:
+            print("Cut position not adjusted")
+            return current_position
 
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import cutter.sample_cut_tool as sct
+import argparse
 
 # /Users/ilyamozerov/Downloads/daddy.mp3
 # /Users/ilyamozerov/Downloads/devil.mp3
 # /Users/ilyamozerov/Downloads/krug.mp3
 
-def cut_samples():
-    tool = sct.MP3SampleCutTool("/Users/ilyamozerov/Downloads/devil.mp3")
+def cut_samples(file_path):
+    tool = sct.MP3SampleCutTool(file_path)
     tool.select_cut_points()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    cut_samples()
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description="Cut samples from an MP3 file")
+    parser.add_argument("file", help="MP3 file to cut samples from")
+
+    args = parser.parse_args()
+    cut_samples(args.file)
