5f152597503f5a02a3f605288c6b226af699f55d
Ilyusha <ilyailiilia@gmail.com>
2023-03-10 22:41:02 +0100

    Fix chunking

--- cutter/automixer/mixers/default_mixer.py
+++ cutter/automixer/mixers/default_mixer.py
@@ -9,7 +9,7 @@ from cutter.automixer.iterators.rolling_window import rolling_window
 from cutter.automixer.utils import calculate_step
 
 
-def _ceate_chunk(config, window):
+def _create_chunk(config, window):
     chunk = AudioSegment.silent(duration=config.sample_length)
     for channel in config.channels_config:
         start_cut = random.choice(window)
@@ -17,7 +17,7 @@ def _ceate_chunk(config, window):
         channel_chunk = band_pass_filer(channel.low_pass, channel.high_pass, channel_chunk)
         chunk = chunk.overlay(channel_chunk)
     if config.sample_speed != 1.0:
-        chunk = change_audioseg_tempo(chunk_in, config.sample_speed, verbose=config.is_verbose_mode_enabled)
+        chunk = change_audioseg_tempo(chunk, config.sample_speed, verbose=config.is_verbose_mode_enabled)
 
     return chunk
 
@@ -28,13 +28,13 @@ class RandomWindowAutoMixer:
         pbar = tqdm(desc="Mixing")
         chunk_length_in_window = calculate_step(config.beats)
         for window in rolling_window(config.beats, config.window_divider):
-            chunk = _ceate_chunk(config, window)
-            chunk.append(chunk, crossfade=0)
-            while len(chunk) < int(chunk_length_in_window):
-                chunk = _ceate_chunk(config, window)
-                chunk.append(chunk, crossfade=0)
-            mix = mix.append(chunk, crossfade=0)
+            chunk1 = _create_chunk(config, window)
+            while len(chunk1) < int(chunk_length_in_window):
+                chunk = _create_chunk(config, window)
+                chunk1 = chunk1.append(chunk, crossfade=0)
+            mix = mix.append(chunk1, crossfade=0)
             pbar.update(len(mix))
         return mix
 
 # amc ss 0.1 s 1.5 c 1,250;500,15000
+# amc ss 2.0 s 0.5 c 1,250;500,15000
